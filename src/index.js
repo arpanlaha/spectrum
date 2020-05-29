3be9865278a652d455275082941e1f13d0371ae5
Arpan Laha <arpanlaha99@gmail.com>
2020-05-29 07:05:19 +0000

    set up format and lint

--- src/index.js
+++ src/index.js
@@ -1,19 +1,24 @@
 import { Spectrum } from "wasm-spectrum";
 import { memory } from "wasm-spectrum/spectrum_bg";
 
-const canvas = document.getElementById("spectrum-canvas");
-const context = canvas.getContext("2d");
+const BYTES_PER_PIXEL = 4;
+
 const width = document.body.clientWidth;
 const height = document.body.clientHeight;
+const numSources = Math.round(Math.sqrt(Math.sqrt(width * height)));
+
+const canvas = document.getElementById("spectrum-canvas");
+const context = canvas.getContext("2d");
 canvas.width = width;
 canvas.height = height;
-const numSources = Math.round(Math.sqrt(Math.sqrt(width * height)));
+
 const spectrum = Spectrum.new(width, height, numSources);
 const spectrumData = spectrum.data();
 const spectrumArray = new Uint8ClampedArray(
   memory.buffer,
   spectrumData,
-  width * height * 4
+  width * height * BYTES_PER_PIXEL
 );
 const spectrumImageData = new ImageData(spectrumArray, width, height);
+
 context.putImageData(spectrumImageData, 0, 0);
