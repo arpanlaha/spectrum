c690b22cbcc35ecd9616c8e78dd633c5529f3a51
Arpan Laha <arpanlaha99@gmail.com>
2020-05-29 06:15:11 +0000

    configure manifest and logo

--- index.js
+++ index.js
@@ -1,31 +1,18 @@
 import { Spectrum } from "wasm-spectrum";
 import { memory } from "wasm-spectrum/spectrum_bg";
 
-console.log("Hello");
-
 const canvas = document.getElementById("spectrum-canvas");
 const context = canvas.getContext("2d");
-// const width = Math.round(document.body.clientWidth / 3);
-const width = Math.round(document.body.clientWidth);
-const height = Math.round(document.body.clientHeight);
+const width = document.body.clientWidth;
+const height = document.body.clientHeight;
 canvas.width = width;
-canvas.height = height;
-// console.log(width, height);
 const numSources = Math.round(Math.sqrt(Math.sqrt(width * height)));
 const spectrum = Spectrum.new(width, height, numSources);
 const spectrumData = spectrum.data();
-// console.log(spectrumData);
 const spectrumArray = new Uint8ClampedArray(
   memory.buffer,
   spectrumData,
   width * height * 4
 );
-
 const spectrumImageData = new ImageData(spectrumArray, width, height);
-const index = 100000;
-// ["r", "g", "b", "a"].forEach((channel, idx) =>
-//   console.log(`${channel}: ${spectrumArray[4 * index + idx]}`)
-// );
 context.putImageData(spectrumImageData, 0, 0);
-
-console.log("goodbye");
