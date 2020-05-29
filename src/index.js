8aa471f913b59240b0967b848393c19c0dba7d3f
Arpan Laha <arpanlaha99@gmail.com>
2020-05-29 20:31:08 +0000

    fix canvas scaling

--- src/index.js
+++ src/index.js
@@ -3,14 +3,22 @@ import { memory } from "wasm-spectrum/spectrum_bg";
 
 const BYTES_PER_PIXEL = 4;
 
-const width = document.body.clientWidth;
-const height = document.body.clientHeight;
+const scale = window.devicePixelRatio;
+
+const { clientWidth, clientHeight } = document.body;
+
+const width = Math.round(document.body.clientWidth * scale);
+const height = Math.round(document.body.clientHeight * scale);
 const numSources = Math.round(Math.sqrt(Math.sqrt(width * height)));
 
 const canvas = document.getElementById("spectrum-canvas");
 const context = canvas.getContext("2d");
+
+canvas.style.width = `${clientWidth}px`;
+canvas.style.height = `${clientHeight}px`;
 canvas.width = width;
 canvas.height = height;
+context.scale(scale, scale);
 
 const spectrum = Spectrum.new(width, height, numSources);
 const spectrumData = spectrum.data();
