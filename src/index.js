c163098e2a1eaf03b05e705e5291c3fca9b944bb
Arpan Laha <arpanlaha99@gmail.com>
2020-05-30 06:17:31 +0000

    it animates

--- src/index.js
+++ src/index.js
@@ -7,12 +7,12 @@ const scale = window.devicePixelRatio;
 
 let { clientWidth, clientHeight } = document.body;
 
-clientWidth = Math.round(clientWidth / 4);
-clientHeight = Math.round(clientHeight / 4);
+clientWidth = Math.round(clientWidth / 3);
+clientHeight = Math.round(clientHeight / 3);
 
 const width = Math.round(clientWidth * scale);
 const height = Math.round(clientHeight * scale);
-const numSources = Math.round(Math.sqrt(Math.sqrt(width * height)) / 4);
+const numSources = Math.round(Math.sqrt(Math.sqrt(width * height)) / 3);
 
 const canvas = document.getElementById("spectrum-canvas");
 const context = canvas.getContext("2d");
@@ -25,23 +25,43 @@ context.scale(scale, scale);
 
 const spectrum = Spectrum.new(width, height, numSources);
 
-for (let i = 0; i < 5; i++) {
-  console.time(`${i}`);
+// for (let i = 0; i < 5; i++) {
+//   console.time(`${i}`);
 
-  if (i == 0) {
-    const spectrumData = spectrum.data();
-    const spectrumArray = new Uint8ClampedArray(
-      memory.buffer,
-      spectrumData,
-      width * height * BYTES_PER_PIXEL
-    );
-    const spectrumImageData = new ImageData(spectrumArray, width, height);
+//   const spectrumData = spectrum.data();
+//   const spectrumArray = new Uint8ClampedArray(
+//     memory.buffer,
+//     spectrumData,
+//     width * height * BYTES_PER_PIXEL
+//   );
+//   const spectrumImageData = new ImageData(spectrumArray, width, height);
 
-    context.putImageData(spectrumImageData, 0, 0);
-  }
+//   context.putImageData(spectrumImageData, 0, 0);
+
+//   spectrum.tick();
+//   spectrum.draw();
+//   console.timeEnd(`${i}`);
+// }
+// canvas.classList.add("show");
+
+const renderLoop = () => {
+  // console.time("hi");
+
+  const spectrumData = spectrum.data();
+  const spectrumArray = new Uint8ClampedArray(
+    memory.buffer,
+    spectrumData,
+    width * height * BYTES_PER_PIXEL
+  );
+  const spectrumImageData = new ImageData(spectrumArray, width, height);
+
+  context.putImageData(spectrumImageData, 0, 0);
 
   spectrum.tick();
   spectrum.draw();
-  console.timeEnd(`${i}`);
-}
-// canvas.classList.add("show");
+  // console.timeEnd("hi");
+
+  window.requestAnimationFrame(renderLoop);
+};
+
+window.requestAnimationFrame(renderLoop);
