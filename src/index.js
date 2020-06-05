50b19f0d90e223753200933e79f18f16d4cc73e9
Arpan Laha <arpanlaha99@gmail.com>
2020-06-05 00:40:24 +0000

    initial functioning interface

--- src/index.js
+++ src/index.js
@@ -5,30 +5,73 @@ const BYTES_PER_PIXEL = 4;
 
 const scale = window.devicePixelRatio;
 
-const width = 1280;
-const height = 720;
-const numSources = 10;
-
-const setWidth = document.getElementById("set-width");
-console.log(setWidth);
-const form = document.getElementById("spectrum-form");
-
-form.addEventListener("submit", (e) => e.preventDefault());
+let spectrum = null;
+let animationId = null;
+let width = 1280;
+let height = 720;
+let numSources = 10;
+let maxWidth = document.body.clientWidth * scale;
+let maxHeight = document.body.clientHeight * scale;
 
 const canvas = document.getElementById("spectrum-canvas");
 const context = canvas.getContext("2d");
 
-canvas.style.width = `${width / scale}px`;
-canvas.style.height = `${height / scale}px`;
-canvas.width = width;
-canvas.height = height;
-context.scale(scale, scale);
+const initSpectrum = () => {
+  canvas.style.width = `${width / scale}px`;
+  canvas.style.height = `${height / scale}px`;
+  canvas.width = width;
+  canvas.height = height;
+  context.scale(scale, scale);
 
-const spectrum = Spectrum.new(width, height, numSources);
+  spectrum = Spectrum.new(width, height, numSources);
+};
 
-let animationId = null;
+initSpectrum(width, height, numSources);
 
-const renderLoop = () => {
+const restartSpectrum = () => {
+  pause();
+  initSpectrum();
+  drawFrame();
+};
+
+const widthText = document.getElementById("width");
+widthText.textContent = width;
+
+const setWidth = document.getElementById("set-width");
+setWidth.min = 100;
+setWidth.max = maxWidth;
+setWidth.value = width;
+setWidth.addEventListener("change", (e) => {
+  const newWidth = e.target.value;
+  width = newWidth;
+  widthText.textContent = width;
+  restartSpectrum();
+});
+
+const heightText = document.getElementById("height");
+heightText.textContent = height;
+
+const setHeight = document.getElementById("set-height");
+setHeight.min = 100;
+setHeight.max = maxHeight;
+setHeight.value = height;
+setHeight.addEventListener("change", (e) => {
+  const newHeight = e.target.value;
+  height = newHeight;
+  heightText.textContent = height;
+  restartSpectrum();
+});
+
+const setNumSources = document.getElementById("set-num-sources");
+setNumSources.min = 2;
+setNumSources.value = numSources;
+setNumSources.addEventListener("input", (e) => {
+  const newNumSources = e.target.value;
+  numSources = newNumSources;
+  restartSpectrum();
+});
+
+const drawFrame = () => {
   const spectrumData = spectrum.data();
   const spectrumArray = new Uint8ClampedArray(
     memory.buffer,
@@ -39,8 +82,12 @@ const renderLoop = () => {
 
   context.putImageData(spectrumImageData, 0, 0);
 
-  spectrum.tick();
   spectrum.draw();
+};
+
+const renderLoop = () => {
+  drawFrame();
+  spectrum.tick();
 
   animationId = window.requestAnimationFrame(renderLoop);
 };
