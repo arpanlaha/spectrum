b0eb1fce28c8f7895e7008fbc04ef4ffd461f202
Arpan Laha <arpanlaha99@gmail.com>
2020-06-14 05:29:23 +0000

    add webassembly check to bootstrap script

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -1,1 +1,32 @@
-import("./index").catch((e) => console.error("Error importing `index.js`:", e));
+/* eslint-disable @typescript-eslint/no-magic-numbers */
+
+// WebAssembly support check derived from https://stackoverflow.com/a/47880734/13172180
+let supported = true;
+try {
+  if (
+    typeof WebAssembly === "object" &&
+    typeof WebAssembly.instantiate === "function"
+  ) {
+    const module = new WebAssembly.Module(
+      Uint8Array.of(0x0, 0x61, 0x73, 0x6d, 0x01, 0x00, 0x00, 0x00)
+    );
+    if (module instanceof WebAssembly.Module) {
+      supported =
+        new WebAssembly.Instance(module) instanceof WebAssembly.Instance;
+    }
+  } else {
+    supported = false;
+  }
+} catch (e) {
+  supported = false;
+}
+
+if (supported) {
+  console.log("hello");
+  import("./index").catch((e) =>
+    console.error("Error importing `index.js`:", e)
+  );
+} else {
+  document.getElementById("canvas-webgl")!.classList.add("hide");
+  document.getElementById("no-wasm")!.classList.remove("hide");
+}
