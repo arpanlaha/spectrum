3eb7601c590b1653ffc753b6b891748b893c1704
Arpan Laha <arpanlaha99@gmail.com>
2020-06-22 05:00:30 +0000

    add more comments

--- src/utils/fps.ts
+++ src/utils/fps.ts
@@ -1,8 +1,24 @@
 const THOUSAND = 1000;
 
+/**
+ * Tracks frames per second, requiring that the `render` method be called on every frame.
+ *
+ * Derived from the [Rust and WebAssembly Game of Life Tutorial - Time Profiling section](https://rustwasm.github.io/book/game-of-life/time-profiling.html).
+ */
 export default class FPS {
+  /**
+   * The HTML element displaying the information.
+   */
   node: HTMLElement;
+
+  /**
+   * A list of frame lengths tracked by the FPS.
+   */
   frames: number[];
+
+  /**
+   * THe time of the last frame.
+   */
   lastFrameTimeStamp: number;
 
   constructor() {
@@ -12,24 +28,22 @@ export default class FPS {
     this.node.textContent = "Waiting to start...";
   }
 
+  /**
+   * Finds the time since the last frame was recorded, updating the internal list of frames and the displayed FPS count.
+   */
   render(): void {
-    // Convert the delta time since the last frame render into a measure
-    // of frames per second.
     const now = performance.now();
     const delta = now - this.lastFrameTimeStamp;
     this.lastFrameTimeStamp = now;
     const frameLength = (1 / delta) * THOUSAND;
 
-    // Save only the latest 100 timings.
     this.frames.push(frameLength);
     if (this.frames.length > 100) {
       this.frames.shift();
     }
 
-    const fps =
-      this.frames.reduce((sum, frame) => sum + frame) / this.frames.length;
-
-    // Render the statistics.
-    this.node.textContent = `${Math.round(fps)} FPS`;
+    this.node.textContent = `${Math.round(
+      this.frames.reduce((sum, frame) => sum + frame) / this.frames.length
+    )} FPS`;
   }
 }
--- src/utils/math.ts
+++ src/utils/math.ts
@@ -4,9 +4,20 @@ const PI_3_2 = Math.PI * 1.5;
 const PI_4 = Math.PI / 4;
 const PI_2 = Math.PI / 2;
 
+/**
+ * Calculates the arctangent, given a quotient in the range [-1, 1].
+ *
+ * Obtained from [IEEE Signal Processing Magazine](http://www-labs.iro.umontreal.ca/~mignotte/IFT2425/Documents/EfficientApproximationArctgFunction.pdf).
+ * @param quotient the minimum of `cos / sin` and `sin / cos`.
+ */
 export const atanApprox = (quotient: number): number =>
   (PI_4 + 0.273 * (1 - Math.abs(quotient))) * quotient;
 
+/**
+ * Calculates the arctangent from the cosine and sine.
+ * @param cos the cosine/x term.
+ * @param sin the sine/y term.
+ */
 export const atan2Approx = (cos: number, sin: number): number => {
   if (Math.abs(cos) > Math.abs(sin)) {
     if (cos < 0) {
